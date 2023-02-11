2ec5c7ac6ba6a8c09ef6ff343f759b2ea55cb00b
lilycuso4 <LilyCuSO4@163.com>
2023-02-10 21:47:37 -0500

    release1.0

--- Ecommerce project/02-Backend/spring-boot-ecommerce/src/main/java/com/lilycuso4/ecommerce/dao/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lilycuso4.ecommerce.dao;
-
-import com.lilycuso4.ecommerce.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Long>{
-}
